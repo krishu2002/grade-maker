3ef8532221bd5e8306b5862fdfd0fe9e22245fa1
krishu2002 <117009023+krishu2002@users.noreply.github.com>
2023-05-27 17:41:44 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,141 @@
+onEvent("button1", "click", function( ) {
+  setScreen("screen22");
+});
+onEvent("button37", "click", function( ) {
+  setScreen("screen2");
+});
+onEvent("image2", "click", function( ) {
+  setScreen("screen3");
+});
+onEvent("image3", "click", function( ) {
+  setScreen("screen3");
+});
+onEvent("button3", "click", function( ) {
+  setImageURL("image18", "icon://fa-times");
+  setScreen("screen4");
+});
+onEvent("button2", "click", function( ) {
+  setImageURL("image18", "icon://fa-check");
+  setScreen("screen4");
+});
+onEvent("button7", "click", function( ) {
+  setImageURL("image16", "icon://fa-check");
+  setScreen("screen6");
+});
+onEvent("button6", "click", function( ) {
+  setImageURL("image16", "icon://fa-times");
+  setScreen("screen6");
+});
+onEvent("button5", "click", function( ) {
+  setImageURL("image17", "icon://fa-check");
+  setScreen("screen5");
+});
+onEvent("button4", "click", function( ) {
+  setImageURL("image17", "icon://fa-times");
+  setScreen("screen5");
+});
+onEvent("button13", "click", function( ) {
+  setScreen("screen7");
+});
+onEvent("button8", "click", function( ) {
+  setImageURL("image27", "icon://fa-times");
+  setScreen("screen9");
+});
+onEvent("button9", "click", function( ) {
+  setImageURL("image27", "icon://fa-check");
+  setScreen("screen9");
+});
+onEvent("button11", "click", function( ) {
+  setImageURL("image25", "icon://fa-check");
+  setScreen("screen10");
+});
+onEvent("button10", "click", function( ) {
+  setImageURL("image25", "icon://fa-times");
+  setScreen("screen10");
+});
+onEvent("button12", "click", function( ) {
+  setImageURL("image26", "icon://fa-check");
+  setScreen("screen11");
+});
+onEvent("button14", "click", function( ) {
+  setImageURL("image26", "icon://fa-times");
+  setScreen("screen11");
+});
+onEvent("button15", "click", function( ) {
+  setScreen("screen7");
+});
+onEvent("button18", "click", function( ) {
+  setScreen("screen3");
+});
+onEvent("button19", "click", function( ) {
+  setScreen("screen8");
+});
+onEvent("button20", "click", function( ) {
+  setScreen("screen12");
+});
+onEvent("button17", "click", function( ) {
+  setImageURL("image39", "icon://fa-times");
+  setScreen("screen13");
+});
+onEvent("button16", "click", function( ) {
+  setImageURL("image39", "icon://fa-check");
+  setScreen("screen13");
+});
+onEvent("button23", "click", function( ) {
+  setImageURL("image38", "icon://fa-check");
+  setScreen("screen14");
+});
+onEvent("button24", "click", function( ) {
+  setImageURL("image38", "icon://fa-times");
+  setScreen("screen14");
+});
+onEvent("button22", "click", function( ) {
+  setImageURL("image37", "icon://fa-check");
+  setScreen("screen15");
+});
+onEvent("button21", "click", function( ) {
+  setImageURL("image37", "icon://fa-times");
+  setScreen("screen16");
+});
+onEvent("button25", "click", function( ) {
+  setScreen("screen16");
+});
+onEvent("button26", "click", function( ) {
+  setScreen("screen16");
+});
+onEvent("button27", "click", function( ) {
+  setScreen("screen7");
+});
+onEvent("button35", "click", function( ) {
+  setScreen("screen17");
+});
+onEvent("button29", "click", function( ) {
+  setImageURL("image51", "icon://fa-times");
+  setScreen("screen18");
+});
+onEvent("button28", "click", function( ) {
+  setImageURL("image51", "icon://fa-check");
+  setScreen("screen18");
+});
+onEvent("button31", "click", function( ) {
+  setImageURL("image49", "icon://fa-check");
+  setScreen("screen19");
+});
+onEvent("button30", "click", function( ) {
+  setImageURL("image49", "icon://fa-times");
+  setScreen("screen19");
+});
+onEvent("button32", "click", function( ) {
+  setImageURL("image50", "icon://fa-check");
+  setScreen("screen20");
+});
+onEvent("button33", "click", function( ) {
+  setImageURL("image50", "icon://fa-times");
+  setScreen("screen20");
+});
+onEvent("button34", "click", function( ) {
+  setScreen("screen7");
+});
+onEvent("button38", "click", function( ) {
+  setScreen("screen21");
+});
